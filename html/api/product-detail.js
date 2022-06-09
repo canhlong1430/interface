1c81562c9e43e4c180dae5558901b628e68c39de
khangle1005 <lephuckhang1005@gmail.com>
2022-06-10 03:48:59 +0700

    đổ data trang product, detail

--- html/api/product-detail.js
+++ html/api/product-detail.js
@@ -1,26 +1,105 @@
-//Url của api
-url = 'https://electronics-api.herokuapp.com/products/54665'
-
-const options = {
-    method: 'GET', //tùy chọn method GET hoặc POST, PUT, DELETE
-    headers: { 'Content-Type': 'application/json' },
-}
-
-/* Quy ước:
-    Đọc data dùng GET
-    Sửa data dùng PUT
-    Thêm data dùng POST
-    Xóa data dùng DELETE
-*/
-
-//Gọi api => trả về dạng Json => chạy loop đổ json ra HTML
-fetch(url, options).then(res => res.json()).then(json => {
-    for (var i = 0; i < json.data.product_images.length; i++) {
-
-        var obj = json.data.product_images[i];
-        var newDiv = document.createElement('div');
-        newDiv.style = "background-image: url(" + "https://salt.tikicdn.com/cache/w300/ts/product/ba/e2/6e/e6195ea67855e398251f7017eb4ceded.jpg" + ");";
-        newDiv.className = 'item-box';
-        // document.getElementById("slider1").appendChild(newDiv)
+$(document).ready(function () {
+    function get(name) {
+        if (name = (new RegExp('[?&]' + encodeURIComponent(name) + '=([^&]*)')).exec(location.search))
+            return decodeURIComponent(name[1]);
     }
+
+    var productId = get('product_id')
+
+    //Url của api
+    url = 'https://electronics-api.herokuapp.com/products/' + productId
+
+    const options = {
+        method: 'GET', //tùy chọn method GET hoặc POST, PUT, DELETE
+        headers: { 'Content-Type': 'application/json' },
+    }
+
+    //Gọi api => trả về dạng Json => chạy loop đổ json ra HTML
+    fetch(url, options).then(res => res.json()).then(json => {
+
+        var newDiv = ``;
+        for (var i = 0; i < json.data.product_images.length; i++) {
+
+            var obj = json.data.product_images[i];
+
+            if (i == 0) {
+                newDiv += `
+                <div class="owl-item active" style="width: 438.4px;">
+												<div style="background-image: url(img/products/shoes/1.jpg);"
+													class="item-box">
+												</div>
+											</div>
+                `
+            }
+            else {
+                newDiv += `
+                <div class="owl-item" style="width: 438.4px;">
+												<div style="background-image: url(img/products/shoes/1.jpg);"
+													class="item-box">
+												</div>
+											</div>
+                `
+            }
+        }
+        // $("#test").children().children().children().append(newDiv)
+
+
+        $(".prod_info h1").text(json.data.name)
+        $(".prod_info p small").text("SKU: " + json.data.sku)
+        $(".prod_info p").append(json.data.short_description)
+        $(".price_main .new_price").text(json.data.price + " đ")
+        $(".price_main .old_price").text(json.data.price + " đ")
+        $("#detail div").html(json.data.description)
+
+        var str = `
+        <div class="row justify-content-between">
+        `
+        for (var i = 0; i < 4; i++) {//Cho hiển thị 4 comment
+            var obj = json.data.product_ratings[i];
+            var user = obj.created_by
+            str += `				
+                <div class="col-lg-6">
+											<div class="review_content">
+												<div class="clearfix add_bottom_10">
+													<span class="rating">
+            `
+            for (var j = 0; j < obj.star_number; j++) {
+                str += `
+                <i class="icon-star"></i>`
+            }
+            str += `
+                 <em>`+ obj.star_number + `/5</em></span>
+													<em>`+ obj.created_at + `</em>
+												</div>
+												<h4>`+ user.name + `</h4>
+												<p>`+ obj.comment + `</p>
+											</div>
+				</div>
+            `
+        }
+
+        str += `</div>`
+        $("#rating div").html(str)
+
+        var str = `<tbody>`
+        var product = json.data.product_options[0]
+        for (var i = 0; i < product.attributes.length; i++) {
+            var attribute = product.attributes[i]
+            str += `
+            <tr>
+            <td><strong>
+            ` + attribute.name + `</strong></td>`
+
+            str += `
+            <td>`+ attribute.attribute_value.value + `</td>
+			</tr>
+            `
+        }
+
+        str += `</tbody>`
+
+        $("#specs").html(str)
+    });
+
+
 });
--- html/api/products.js
+++ html/api/products.js
@@ -1,41 +1,45 @@
-//Url của api
-var page = 1
-var limit = '9'
-var offset = page - 1
-url = 'https://electronics-api.herokuapp.com/products?limit=' + limit + '&offset=' + offset.toString()
+$(document).ready(function () {
+	//Url của api
+	var page = 1
+	var limit = '9'
+	var offset = page - 1
+	url = 'https://electronics-api.herokuapp.com/products?limit=' + limit + '&offset=' + offset.toString()
 
-const options = {
-    method: 'GET', //tùy chọn method GET hoặc POST, PUT, DELETE
-    headers: { 'Content-Type': 'application/json' },
-}
+	const options = {
+		method: 'GET', //tùy chọn method GET hoặc POST, PUT, DELETE
+		headers: { 'Content-Type': 'application/json' },
+	}
 
-/* Quy ước:
-    Đọc data dùng GET
-    Sửa data dùng PUT
-    Thêm data dùng POST
-    Xóa data dùng DELETE
-*/
+	/* Quy ước:
+		Đọc data dùng GET
+		Sửa data dùng PUT
+		Thêm data dùng POST
+		Xóa data dùng DELETE
+	*/
 
-//Gọi api => trả về dạng Json => chạy loop đổ json ra HTML
-fetch(url, options).then(res => res.json()).then(json => {
-    for (var i = 0; i < json.data.length; i++) {
+	// Tham khảo
+	// https://suntech.edu.vn/http-request-trong-javascript-voi-fetch-api.sunpost.html
 
-        //Hiển thị tên sản phẩm mra HTML
-        var obj = json.data[i];
-        var newDiv = document.createElement('div');
-        newDiv.className = 'col-6 col-md-4'
-        newDiv.innerHTML = `
+	//Gọi api => trả về dạng Json => chạy loop đổ json ra HTML
+	fetch(url, options).then(res => res.json()).then(json => {
+		for (var i = 0; i < json.data.length; i++) {
+
+			//Hiển thị tên sản phẩm mra HTML
+			var obj = json.data[i];
+			var newDiv = document.createElement('div');
+			newDiv.className = 'col-6 col-md-4'
+			newDiv.innerHTML = `
         						<div class="grid_item">
         							<span class="ribbon off">-30%</span>
         							<figure>
-        								<a href="product-detail-1.html">
+										<a href="product-detail-2.html?product_id=` + obj.id + `">
         									<img class="img-fluid lazy"
         										src="` + obj.thumbnail_url + `"
         										data-src="` + obj.thumbnail_url + `" alt="">
         								</a>
         								<!-- <div data-countdown="2019/05/15" class="countdown"></div> -->
         							</figure>
-        							<a href="product-detail-1.html">
+        							<a href="product-detail-2.html?product_id=` + obj.id + `">
         								<h3>`+ obj.name + `</h3>
         							</a>
         							<div class="price_box">
@@ -55,9 +59,10 @@ fetch(url, options).then(res => res.json()).then(json => {
         							</ul>
         						</div>     
         `;
-        document.getElementById("list_product").appendChild(newDiv)
-    }
-});
+			document.getElementById("list_product").appendChild(newDiv)
+		}
+	});
 
-// Tham khảo
-// https://suntech.edu.vn/http-request-trong-javascript-voi-fetch-api.sunpost.html
+
+
+});
