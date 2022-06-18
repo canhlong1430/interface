20e63a6a167116120cea52277402f64bb2971bdc
khangle1005 <lephuckhang1005@gmail.com>
2022-06-18 22:50:14 +0700

    update

--- html/api/login.js
+++ html/api/login.js
@@ -56,105 +56,111 @@ $(document).ready(function () {
         var birthday = "1999-01-01"
         var phone_number = $("#phone_number").val()
         var name = $("#name").val()
-        if (email == "" || password == "" || avatar == "" || gender == "" || phone_number == "") {
-            $('.new_client').append(' <span style="color:red"> [Lỗi - Bạn cần điền đầy đủ thông tin!]</span>')
-            setTimeout(function () {
-                $('.new_client > span').hide()
-            }, 1000);
+        var confirm = $("#confirm-reg-pass").val()
+
+        if (password == confirm) {
+            if (email == "" || password == "" || avatar == "" || gender == "" || phone_number == "" || password == "" || confirm == "") {
+                $('.new_client').append(' <span style="color:red"> [Lỗi - Bạn cần điền đầy đủ thông tin!]</span>')
+                setTimeout(function () {
+                    $('.new_client > span').hide()
+                }, 1000);
+            } else {
+                const options = {
+                    method: 'POST',
+                    headers: { 'Content-Type': 'application/json' },
+                    body: JSON.stringify({
+                        data:
+                        {
+                            name: name,
+                            avatar: avatar,
+                            email: email,
+                            gender: gender,
+                            birthday: birthday,
+                            phone_number: phone_number,
+                            password: password
+                        }
+                    })
+                };
+
+                url = 'http://localhost:1323/register'
+
+                var status
+                fetch(url, options)
+                    .then((res) => {
+                        console.log(res.status);
+                        status = res.status
+                        return res.json();
+                    })
+                    .then(data => {
+                        localStorage.setItem('token', data.token);
+                        console.log('Success:', data.token)
+                        if (status == 200) {
+                            $('.new_client').append(' <span style="color:green"> [Đăng ký thành công!]</span>')
+                            setTimeout(function () {
+                                $('.new_client > span').hide()
+                                window.location.href = '/html/account.html';
+                            }, 1000);
+                        }
+                        if (status != 200) {
+                            // alert("Email hoặc mật khẩu không đúng. Xin thử lại!")
+                            // window.location.href = '/html/account.html';
+
+                            $('.new_client').append(' <span style="color:red"> [Lỗi - Đăng ký không thành công, thử lại với email khác!]</span>')
+                            setTimeout(function () {
+                                $('.new_client > span').hide()
+                            }, 1000);
+                        }
+                    })
+                    .catch(error => {
+                        console.log('Error:', error)
+                        $('.new_client').append(' <span style="color:red"> [Lỗi - Đăng ký không thành công, Có lỗi xảy ra!]</span>')
+                        setTimeout(function () {
+                            $('.new_client > span').hide()
+                        }, 1000);
+                    });
+            }
         } else {
-            const options = {
-                method: 'POST',
+            $('.new_client').append(' <span style="color:red"> [Xác nhận mật khẩu không đúng!]</span>')
+        }
+
+        $("#forget-password").click(function () {
+            var email = $("#email_forgot").val()
+
+            const fpOptions = {
+                method: 'PUT',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
                     data:
                     {
-                        name: name,
-                        avatar: avatar,
                         email: email,
-                        gender: gender,
-                        birthday: birthday,
-                        phone_number: phone_number,
-                        password: password
                     }
                 })
             };
 
-            url = 'http://localhost:1323/register'
+            fpUrl = 'http://localhost:1323/users/forget_password'
 
             var status
-            fetch(url, options)
+            fetch(fpUrl, fpOptions)
                 .then((res) => {
                     console.log(res.status);
                     status = res.status
-                    return res.json();
-                })
-                .then(data => {
-                    localStorage.setItem('token', data.token);
-                    console.log('Success:', data.token)
+
                     if (status == 200) {
-                        $('.new_client').append(' <span style="color:green"> [Đăng ký thành công!]</span>')
+                        $('.client').append(' <span style="color:green"> [Mật khẩu đã được gửi!]</span>')
                         setTimeout(function () {
-                            $('.new_client > span').hide()
-                            window.location.href = '/html/account.html';
+                            $('.client > span').hide()
+                            window.location.reload()
                         }, 1000);
                     }
                     if (status != 200) {
-                        // alert("Email hoặc mật khẩu không đúng. Xin thử lại!")
-                        // window.location.href = '/html/account.html';
-
-                        $('.new_client').append(' <span style="color:red"> [Lỗi - Đăng ký không thành công, thử lại với email khác!]</span>')
+                        $('.client').append(' <span style="color:red"> [Lỗi - Có lỗi xảy ra!]</span>')
                         setTimeout(function () {
-                            $('.new_client > span').hide()
+                            $('.client > span').hide()
                         }, 1000);
                     }
-                })
-                .catch(error => {
-                    console.log('Error:', error)
-                    $('.new_client').append(' <span style="color:red"> [Lỗi - Đăng ký không thành công, Có lỗi xảy ra!]</span>')
-                    setTimeout(function () {
-                        $('.new_client > span').hide()
-                    }, 1000);
+
+                    return res.json();
                 });
-        }
-    });
-
-    $("#forget-password").click(function () {
-        var email = $("#email_forgot").val()
-
-        const fpOptions = {
-            method: 'PUT',
-            headers: { 'Content-Type': 'application/json' },
-            body: JSON.stringify({
-                data:
-                {
-                    email: email,
-                }
-            })
-        };
-
-        fpUrl = 'http://localhost:1323/users/forget_password'
-
-        var status
-        fetch(fpUrl, fpOptions)
-            .then((res) => {
-                console.log(res.status);
-                status = res.status
-
-                if (status == 200) {
-                    $('.client').append(' <span style="color:green"> [Mật khẩu đã được gửi!]</span>')
-                    setTimeout(function () {
-                        $('.client > span').hide()
-                        window.location.reload()
-                    }, 1000);
-                }
-                if (status != 200) {
-                    $('.client').append(' <span style="color:red"> [Lỗi - Có lỗi xảy ra!]</span>')
-                    setTimeout(function () {
-                        $('.client > span').hide()
-                    }, 1000);
-                }
-
-                return res.json();
-            });
+        });
     });
 });
